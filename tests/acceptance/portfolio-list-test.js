5841a887e235fd1f1b3225fe4619d1807a0d3664
Denis Toledo <contato@denistoledo.com.br>
2018-11-30 02:02:57 -0200

    planning the app and creating the acceptance test

---
+++ tests/acceptance/portfolio-list-test.js
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | portfolio list', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show portfolio as the home page', async function(assert) {
+  });
+
+  test('should link to the information about me', async function(assert) {
+  });
+
+  test('should link to the contact page', async function(assert) {
+  });
+
+  test('should list all works', async function(assert) {
+  });
+
+  test('should filter the list of works by type', async function(assert) {
+  });
+
+  test('should show details for a selected work', async function(assert) {
+  });
+
+  test('', async function(assert) {
+  });
+});
