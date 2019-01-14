a04affa9c390c0d92c66a13714780bfa9db48e65
Denis Toledo <contato@denistoledo.com.br>
2019-01-13 23:29:14 -0200

    using mirage for tests

--- config/environment.js
+++ config/environment.js
@@ -30,9 +30,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
-    // ENV['ember-cli-mirage'] = {
-    //   enabled: false
-    // }
+    ENV['ember-cli-mirage'] = {
+      enabled: false
+    }
   }
 
   if (environment === 'test') {
@@ -45,6 +45,10 @@ module.exports = function(environment) {
 
     ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
+
+    ENV['ember-cli-mirage'] = {
+      enabled: true
+    }
   }
 
   if (environment === 'production') {
